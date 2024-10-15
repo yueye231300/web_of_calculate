8cf3c845a4afc4e50fcb0ab7a8c0a5da7d8bcc31
yue ye <1650958692@qq.com>
2024-10-15 23:09:23 +0800

    origin git——chapter

--- web.py
+++ web.py
@@ -107,7 +107,7 @@ if zdm_path is not None:
             limitation = limit(zdm, qiao)
             zdm_plot = zdm_z_len.iloc[:limitation]
             zdm_path_1 = zdm_path[-10:-8]
-            st.write(zdm_path_1)
+            st.write(zdm_path)
             if chapter is not None:
                 for i in range(len(bridge_path_1['name'])):
                     if bridge_path_1['name'][i]==zdm_path_1:
