566c707eac006b80f31bc24804863baee7b0e2c2
yue ye <1650958692@qq.com>
2024-10-15 16:49:40 +0800

    origin git——change for the limitation

--- web.py
+++ web.py
@@ -79,7 +79,7 @@ st.subheader('对应雍水图像')
 if zdm is not None:
     if jmd is not None:
         if qiao is not None:
-            # name = zdm.name()
+            st.write(zdm.name())
             zdm = pd.read_csv(zdm)
             # st.write(name)
             beginner = zdm.iloc[0, 0:2]
