99cafc378458d3c21653c725cd007c7c38bf324c
yue ye <1650958692@qq.com>
2024-10-15 23:49:41 +0800

    origin git——chapter

--- web.py
+++ web.py
@@ -102,9 +102,7 @@ if zdm_path is not None:
             hdm = qiao
             jmd_z_len = hebing(near_file, len_1)
             zdm_z_len = hebing(zdm, zdm)
-            qiao.sort_values(by='z', inplace=True,ignore_index=False)
-            st.write(qiao)
-            qiao_lower = qiao['z'][0]
+            qiao_lower = qiao['z'].min()
             zdm_yongshui = pd.DataFrame()
             limitation = limit(zdm, hdm)
             zdm_plot = zdm_z_len.iloc[:limitation]
